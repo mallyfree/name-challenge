1f62bc87bbafa2448d4355dacd19adae76662d97
jamaal freeman <mallyfreee@gmail.com>
2015-09-22 09:56:41 -0400

    work on person class

--- name_test.rb
+++ name_test.rb
@@ -1,5 +1,24 @@
 require 'minitest/autorun'
 
+class Person
+  attr_accessor :name
+# def name
+# @name
+#def name=new_name
+# @name =new_name
+#end
+  def initialize given_name
+    self.name = given_name
+  end
+end
+
+  def angry_name
+    self.name.upcase + !!!
+  end
+end
+binding.pry
+
+
 class PersonTest < Minitest::Test
   def test_people_know_their_name
     tom = Person.new "Tom Crawford"
